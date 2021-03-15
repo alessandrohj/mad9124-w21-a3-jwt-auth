0cdc8acf1c12048f060eea3b8f4109a7dde2b47d
deje0014 <72152264+deje0014@users.noreply.github.com>
2021-03-15 17:27:23 -0400

    Created server and MongoDB connection

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // Don't forget to use NPM to install Express and Mongoose.
+import morgan from 'morgan'
+import express from 'express'
+import sanitizeMongo from 'express-mongo-sanitize'
+import {User} from './models/index.js'
+import authRouter from './routes/auth/index.js'
+
+import connectDatabase from './startup/connectDatabase.js'
+connectDatabase()
+
+
+const app = express()
+
+app.use(morgan('tiny'))
+app.use(express.json())
+app.use(sanitizeMongo())
+
+// 
+
+
+export default app
